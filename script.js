df0b1ac31c92a3ada9439645815c4e8ed0d0aa60
Marcin Gorniakowski <marcin@gorniakowski.pl>
2021-09-28 09:06:53 +0200

    9

--- script.js
+++ script.js
@@ -95,3 +95,19 @@ Array.from(aboutMeTextContent).forEach((char) => {
   });
 });
 //End of About Me Text
+
+// Projects
+const projects = document.querySelectorAll(".project");
+
+projects.forEach((project) => {
+  project.addEventListener("mouseenter", () => {
+    project.firstElementChild.style.top = `-${
+      project.firstElementChild.offsetHeight - project.offsetHeight + 20
+    }px`;
+  });
+
+  project.addEventListener("mouseleave", () => {
+    project.firstElementChild.style.top = "2rem";
+  });
+});
+// End of Projects
