390ba7b6e62b876e3c65554b889cff054a45daaf
Marcin Gorniakowski <marcin@gorniakowski.pl>
2021-09-28 18:47:40 +0200

    12

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ const container = document.querySelector(".container");
 const projects = document.querySelectorAll(".project");
 const projectHideBtn = document.querySelector(".project-hide-btn");
 
-projects.forEach((project) => {
+projects.forEach((project, i) => {
   project.addEventListener("mouseenter", () => {
     project.firstElementChild.style.top = `-${
       project.firstElementChild.offsetHeight - project.offsetHeight + 20
@@ -136,5 +136,38 @@ projects.forEach((project) => {
   });
 
   // End of Big Project Image
+
+  if (i >= 6) {
+    project.style.cssText = "display:none; opacity: 0";
+  }
 });
+
+// Project Button
+const projectsBtn = document.querySelector(".projects-btn");
+const projectsBtnText = document.querySelector(".projects-btn span");
+
+let showHideBool = true;
+
+projectsBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  projectsBtn.firstElementChild.nextElementSibling.classList.toggle("change");
+
+  projects.forEach((project, i) => {
+    if (i >= 6) {
+      if (showHideBool) {
+        project.style.display = "flex";
+        project.style.opacity = "1";
+        projectsBtnText.textContent = "Show Less";
+      } else {
+        project.style.display = "none";
+        project.style.opacity = "0";
+        projectsBtnText.textContent = "Show More";
+      }
+    }
+  });
+  showHideBool = !showHideBool;
+});
+// End of Project Button
+
 // End of Projects
