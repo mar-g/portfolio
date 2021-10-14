c329ea68587b1d96e85b97509f9b46bf20c44cc3
Marcin Gorniakowski <marcin@gorniakowski.pl>
2021-10-14 11:35:50 +0200

    30

--- script.js
+++ script.js
@@ -372,20 +372,46 @@ const messages = document.querySelectorAll(".message");
 const error = (input, message) => {
   input.nextElementSibling.classList.add("error");
   input.nextElementSibling.textContent = message;
+};
 
-}
+const success = (input) => {
+  input.nextElementSibling.classList.remove("error");
+};
 
 const checkRequiredFields = (inputArr) => {
-  inputArr.forEach(input => {
-    if(input.value.trim() === "") {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
       error(input, `${input.id} is required`);
     }
-  })
+  });
+};
+
+const checkLength = (input, min) => {
+  if (input.value.trim().length < min) {
+    error(input, `${input.id} must be at least ${min} characters`);
+  } else {
+    success(input);
+  }
+};
+
+const checkEmail = (input) => {
+  const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if(regEx.test(input.value.trim())) {
+    success(input);
+  } else {
+    error(input, "Email is not valid");
+  }
 }
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  checkLength(username, 2);
+  checkLength(subject, 2);
+  checkLength(message, 10);
+  checkEmail(email);
   checkRequiredFields([username, email, subject, message]);
-})
+});
 // End of Form Validation
 // End of Section 5
